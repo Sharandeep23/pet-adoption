7155d991ecd3599e2405d5d5b0134457fd931d48
Sharandeep <sharandeep23@yahoo.com>
2025-03-21 21:31:19 +0600

    Did dynamic Pet area with <template> and DocumentFragment

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const wrapper = document.createDocumentFragment();
+
 async function start() {
   const weatherResponseObj = await fetch(
     'https://api.weather.gov/gridpoints/MFL/110,50/forecast'
@@ -15,21 +17,22 @@ async function petsArea() {
     'https://learnwebcode.github.io/bootcamp-pet-data/pets.json'
   );
   const petsDataArr = await petsResponseObj.json();
-  const petsEl = document.querySelector('#pets');
   for (const { name, description, birthYear, photo, species } of petsDataArr) {
-    petsEl.innerHTML += `<div class="pet-card">
-            <div class="pet-card-text">
-              <h3>${name}</h3>
-              <p class="pet-description">
-                ${description}
-              </p>
-              <p class="pet-age">${new Date().getFullYear() - birthYear}</p>
-            </div>
-            <div class="pet-card-photo">
-              <img src="${photo}" alt="A ${species}" />
-            </div>
-          </div>`;
+    const petCardClone = document
+      .querySelector('#pet-card-template')
+      .content.cloneNode(true);
+
+    petCardClone.querySelector('h3').textContent = name;
+    petCardClone.querySelector('.pet-description').textContent = description;
+    petCardClone.querySelector('.pet-age').textContent = `${
+      new Date().getFullYear() - birthYear
+    } years old`;
+    petCardClone.querySelector('img').src = photo;
+    petCardClone.querySelector('img').alt = `A ${name}`;
+
+    wrapper.append(petCardClone);
   }
+  document.querySelector('#pets').append(wrapper);
 }
 
 petsArea();
