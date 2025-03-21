161a918a6f0e791aa402be30e12ad1821c9c922b
Sharandeep <sharandeep23@yahoo.com>
2025-03-21 16:47:40 +0600

    rendering dynamic pet area

--- app.js
+++ app.js
@@ -11,13 +11,25 @@ async function start() {
 start();
 
 async function petsArea() {
-  const petsPromise = await fetch(
+  const petsResponseObj = await fetch(
     'https://learnwebcode.github.io/bootcamp-pet-data/pets.json'
   );
-  const petsData = await petsPromise.json();
-  petsData.forEach((pet) => {
-    console.log(pet.name);
-  });
+  const petsDataArr = await petsResponseObj.json();
+  const petsEl = document.querySelector('#pets');
+  for (const { name, description, birthYear, photo, species } of petsDataArr) {
+    petsEl.innerHTML += `<div class="pet-card">
+            <div class="pet-card-text">
+              <h3>${name}</h3>
+              <p class="pet-description">
+                ${description}
+              </p>
+              <p class="pet-age">${new Date().getFullYear() - birthYear}</p>
+            </div>
+            <div class="pet-card-photo">
+              <img src="${photo}" alt="A ${species}" />
+            </div>
+          </div>`;
+  }
 }
 
 petsArea();
